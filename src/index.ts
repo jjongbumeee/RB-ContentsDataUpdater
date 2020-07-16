434c6a7ac3f5bd4f47faeeafe0c9e9edd9bfbe73
Park Jein <55627999+pjainxido@users.noreply.github.com>
2020-07-16 15:26:31 +0900

    sample code 추가, sheet_json 값 수정

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import _ from 'lodash';
-
+import tagJSON from '../sheet_json/tag.json';   //이런 방식으로 export한 json 파일 import
 
 let tmp: any[] = [ {id:123, name:'화'}, {id:123, name:'이'}, {id:123, name:'팅'}];
 
 
-console.log(_.filter(tmp, _.matches({name:'화'})));
+console.log(_.filter(tmp, _.matches({name:'화'}))); //lodash function
+
+const sampleFunction = (tag: any[]) => {     //화살표 함수
+    console.log(tag.length);
+    console.log(_.filter(tag, _.matches({name: '유모차'}))) // 해당 tag array 내에서 name 값이 유모차인 값을 필터
+    for(let i in tag){
+        console.log(tag[i].id);
+        //do something 
+    }
+
+}
 
 
 
 console.log('hello world?');
+
+
+sampleFunction(tagJSON);
