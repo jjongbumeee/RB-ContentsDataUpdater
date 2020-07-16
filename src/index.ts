d6d03b68b90c7e14b8a94ccffaca414c5bc28f68
Park Jein <55627999+pjainxido@users.noreply.github.com>
2020-07-16 12:35:56 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import _ from 'lodash';
+
+
+let tmp: any[] = [ {id:123, name:'화'}, {id:123, name:'이'}, {id:123, name:'팅'}];
+
+
+console.log(_.filter(tmp, _.matches({name:'화'})));
+
+
+
 console.log('hello world?');
