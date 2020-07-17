3ffc272fc3aa67baf58226ede4c42497420dfba5
DanielJung <danieltiger60@gmail.com>
2020-07-17 14:47:24 +0900

    goods parsing done

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,63 @@
 import _ from 'lodash';
 import tagJSON from '../sheet_json/tag.json';   //이런 방식으로 export한 json 파일 import
-
+import goodsJSON from '../data/goods.json';
 let tmp: any[] = [ {id:123, name:'화'}, {id:123, name:'이'}, {id:123, name:'팅'}];
-
 class OutputSample{
-    
-
+    id: number;
+    date: string;
+    date_gmt: string;
+    type: string;
+    link: string;
+    title: object;
+    content: object;
+    featured_media: number;
+    categories: number[];
+    tags: number[];
+    featured_image_src: string;
+    constructor(id, date, date_gmt, type, link, title, content, featured_media, categories, tags, featured_image_src){
+        this.id = id;
+        this.date = date;
+        this.date_gmt = date_gmt;
+        this.type = type;
+        this.link = link;
+        this.title = title;
+        this.content = content;
+        this.featured_media = featured_media;
+        this.categories = categories;
+        this.tags = tags;
+        this.featured_image_src = featured_image_src;
+    }
 }
 
 console.log(_.filter(tmp, _.matches({name:'화'}))); //lodash function
-
 const sampleFunction = (tag: any[]) => {     //화살표 함수
-    console.log(tag.length);
     let outputArr: OutputSample[] = [];
-    
-    console.log(_.filter(tag, _.matches({name: '유모차'}))) // 해당 tag array 내에서 name 값이 유모차인 값을 필터
+    //console.log(_.filter(tag, _.matches({name: '유모차'}))) // 해당 tag array 내에서 name 값이 유모차인 값을 필터
+
     for(let i in tag){
-        console.log(tag[i].id);
-        //do something 
-        let tmp: OutputSample; 
-
+        if(tag[i].category0 == null) continue;
+        
         //tmp 값을 넣는 로직
-
-
+        let now = tag[i];
+        let content = {}, site = "", category = [180];
+        if (now.site === "naver-blog") {
+            content = { rendered: now.fullUrl, protected: false }
+            site = "naver";
+        }
+        else{ 
+            content = { rendered: now.embedHTML, protected: false };
+            site = "youtube";
+        }
+        category.push(now.category1);
+        category.push(now.category2);
+        let tmp = new OutputSample(now.category0, now.CreatedAt, now.UpdatedAt,
+            site, now.fullUrl, {rendered: site +" "+ now.searchKeyword},
+            content, 0, category, [now.Tag1], now.thumbnailUrl);
+        
         outputArr.push(tmp);
+        console.log(tmp);
+        break;
     }
 
 }
-
-
-
-console.log('hello world?');
-
-
-sampleFunction(tagJSON);
+sampleFunction(goodsJSON);
